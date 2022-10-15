bd94984d27436eccc8ce6991f4aa8d96c7f13923
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-10-16 00:12:56 +0200

    fixing deployment error

--- pages/index.js
+++ pages/index.js
@@ -17,16 +17,11 @@ export default function Home() {
         <title>AAST | E-Learning</title>
         <meta name="description" content="Platform to enrich your Learning Experience" />
         <link rel="icon" href="/favicon.ico" />
-
-        <script
-          src="https://code.jquery.com/jquery-3.6.0.min.js"
-          integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-          crossorigin="anonymous"></script>
       </Head>
 
 
       <header>
-      <AlertBadge />
+        <AlertBadge />
         <Navbar />
 
       </header>
